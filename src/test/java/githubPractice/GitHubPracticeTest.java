202c8449cf4f9cd6c8db9b11bb0193d7ec756121
Epili-Suman <epilisuman2@gmail.com>
2026-02-26 16:27:42 +0530

    upload git

---
+++ src/test/java/githubPractice/GitHubPracticeTest.java
@@ -0,0 +1,12 @@
+package githubPractice;
+
+import org.testng.annotations.Test;
+
+public class GitHubPracticeTest
+{
+	@Test
+	public void githubPracticeTest()
+	{
+		System.out.println("hii git hub");
+	}
+}
