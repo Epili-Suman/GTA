cef0da967762aa685bcbee42f9597659d851bf29
Epili-Suman <epilisuman2@gmail.com>
2026-02-27 12:19:25 +0530

    new class added

--- src/test/java/mavenPractice/MavenPracticeTest.java
+++ src/test/java/mavenPractice/MavenPracticeTest.java
@@ -8,6 +8,5 @@ public class MavenPracticeTest
 	public void mavenPracticeTest()
 	{
 		System.out.println("hii maven");
-		System.out.println(System.getProperty("browser"));
 	}
 }
---
+++ src/test/java/mavenPractice/NewMavenTest.java
@@ -0,0 +1,5 @@
+package mavenPractice;
+
+public class NewMavenTest {
+
+}
